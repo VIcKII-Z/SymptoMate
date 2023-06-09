0e997d4010ff2289612b63142c4d839f6b207eb9
VIcKII-Z <zouweijin98@163.com>
2023-06-08 17:13:53 -0700

    one push

---
+++ backend/config.py
@@ -0,0 +1,12 @@
+"""config.py"""
+import os
+
+# Get the Flask app's root directory
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Specify the path to key resources
+DATABASE_PATH = os.path.join(APP_ROOT, '..', 'database', 'database.sqlite')
+PROMPT_PATH = os.path.join(APP_ROOT, '..', 'database', 'prompt.txt')
+PROMPT_PATH_LINK = os.path.join(APP_ROOT, '..', 'database', 'prompt_link.txt')
+API_KEYS = []
+MAX_TRIALS = len(API_KEYS)
